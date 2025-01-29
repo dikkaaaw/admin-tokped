6654b14e80672632db8d3a5097ba77a5639ad52a
Dika Wicaksono <dhkwck00@gmail.com>
2025-01-30 02:43:59 +0700

    Add main layout for admin dashboard

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,113 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+     <!-- Brand Logo -->
+     <a href="#" class="brand-link">
+         {{-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+             style="opacity: .8"> --}}
+         <span class="brand-text font-weight-light">AdminLTE 3</span>
+     </a>
+
+     <!-- Sidebar -->
+     <div class="sidebar">
+         <div class="pb-3 mt-3 mb-3 user-panel d-flex">
+             <div class="image">
+                 <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+             </div>
+             <div class="info">
+                 <a href="#" class="d-block">Alexander Pierce</a>
+             </div>
+         </div>
+
+         <!-- SidebarSearch Form -->
+         <div class="form-inline">
+             <div class="input-group" data-widget="sidebar-search">
+                 <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                     aria-label="Search">
+                 <div class="input-group-append">
+                     <button class="btn btn-sidebar">
+                         <i class="fas fa-search fa-fw"></i>
+                     </button>
+                 </div>
+             </div>
+         </div>
+
+         <!-- Sidebar Menu -->
+         <nav class="mt-2">
+             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                 data-accordion="false">
+                 <li class="nav-header">DASHBOARD</li>
+                 <li class="nav-item">
+                     <a href="{{ URL('admin/dashboard') }}" class="nav-link">
+                         <i class="nav-icon fas fa-home"></i>
+                         <p>
+                             Dashboard
+                         </p>
+                     </a>
+                 </li>
+
+                 <li class="nav-header">PRODUCTS</li>
+                 <li class="nav-item">
+                     <a href="#" class="nav-link">
+                         <i class="nav-icon fas fa-box"></i>
+                         <p>
+                             Product
+                             <i class="right fas fa-angle-left"></i>
+                         </p>
+                     </a>
+                     <ul class="nav nav-treeview">
+                         <li class="nav-item">
+                             <a href="{{ URL('admin/product') }}" class="nav-link">
+                                 <i class="fas fa-list nav-icon"></i>
+                                 <p>Data Product</p>
+                             </a>
+                         </li>
+                         <li class="nav-item">
+                             <a href="#" class="nav-link">
+                                 <i class="fas fa-plus nav-icon"></i>
+                                 <p>Tambah Product</p>
+                             </a>
+                         </li>
+                         <li class="nav-item">
+                             <a href="#" class="nav-link">
+                                 <i class="fas fa-edit nav-icon"></i>
+                                 <p>Edit Product</p>
+                             </a>
+                         </li>
+                     </ul>
+                 </li>
+
+                 <li class="nav-header">USERS</li>
+                 <li class="nav-item">
+                     <a href="{{ URL('admin/user') }}" class="nav-link">
+                         <i class="nav-icon fas fa-users"></i>
+                         <p>
+                             Data User
+                         </p>
+                     </a>
+                 </li>
+
+                 <li class="nav-header">ORDERS</li>
+                 <li class="nav-item">
+                     <a href="#" class="nav-link">
+                         <i class="nav-icon fas fa-box"></i>
+                         <p>
+                             Data Order
+                         </p>
+                     </a>
+                 </li>
+
+                 <li class="nav-header">TRANSACTIONS</li>
+                 <li class="nav-item">
+                     <a href="{{ URL('admin/transaction') }}" class="nav-link">
+                         <i class="nav-icon fas fa-money-bill"></i>
+                         <p>
+                             Transaction
+                         </p>
+                     </a>
+                 </li>
+             </ul>
+         </nav>
+         <!-- /.sidebar-menu -->
+     </div>
+     <!-- /.sidebar -->
+ </aside>

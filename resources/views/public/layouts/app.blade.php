c37227f28912964b2fa8dd5857a77ec94603567b
Dika Wicaksono <dhkwck00@gmail.com>
2025-01-31 22:18:01 +0700

    Add Font Awesome stylesheet to layout for icon support

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FoodMart</title>
 
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <!-- Swiper CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
     <!-- Bootstrap CSS -->

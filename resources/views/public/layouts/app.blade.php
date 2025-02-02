b0a9bcdc7294aa84705ad942d0e27ee23b051e5b
Dika Wicaksono <dhkwck00@gmail.com>
2025-02-03 04:28:05 +0700

    Add CSRF token meta tag and update Font Awesome stylesheet for improved security and styling

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -4,11 +4,15 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FoodMart</title>
 
     {{-- Font Awesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/regular.min.css"
+        integrity="sha512-8hM9a+2hrLBhOuB3uiy+QIXBsu6Qk+snsP1CboFQW6pdt/yYz0IcDp/+CGv5m39r9doGUc/zw6aBpyLF6XFgzg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Swiper CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
     <!-- Bootstrap CSS -->
@@ -24,104 +28,6 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
         rel="stylesheet" />
-    <script data-cfasync="false" nonce="47407772-8439-4371-a211-058fad88f43b">
-        try {
-            (function(w, d) {
-                ! function(j, k, l, m) {
-                    if (j.zaraz) console.error("zaraz is loaded twice");
-                    else {
-                        j[l] = j[l] || {};
-                        j[l].executed = [];
-                        j.zaraz = {
-                            deferred: [],
-                            listeners: []
-                        };
-                        j.zaraz._v = "5850";
-                        j.zaraz._n = "47407772-8439-4371-a211-058fad88f43b";
-                        j.zaraz.q = [];
-                        j.zaraz._f = function(n) {
-                            return async function() {
-                                var o = Array.prototype.slice.call(arguments);
-                                j.zaraz.q.push({
-                                    m: n,
-                                    a: o
-                                })
-                            }
-                        };
-                        for (const p of ["track", "set", "debug"]) j.zaraz[p] = j.zaraz._f(p);
-                        j.zaraz.init = () => {
-                            var q = k.getElementsByTagName(m)[0],
-                                r = k.createElement(m),
-                                s = k.getElementsByTagName("title")[0];
-                            s && (j[l].t = k.getElementsByTagName("title")[0].text);
-                            j[l].x = Math.random();
-                            j[l].w = j.screen.width;
-                            j[l].h = j.screen.height;
-                            j[l].j = j.innerHeight;
-                            j[l].e = j.innerWidth;
-                            j[l].l = j.location.href;
-                            j[l].r = k.referrer;
-                            j[l].k = j.screen.colorDepth;
-                            j[l].n = k.characterSet;
-                            j[l].o = (new Date).getTimezoneOffset();
-                            if (j.dataLayer)
-                                for (const t of Object.entries(Object.entries(dataLayer).reduce(((u, v) => ({
-                                        ...u[1],
-                                        ...v[1]
-                                    })), {}))) zaraz.set(t[0], t[1], {
-                                    scope: "page"
-                                });
-                            j[l].q = [];
-                            for (; j.zaraz.q.length;) {
-                                const w = j.zaraz.q.shift();
-                                j[l].q.push(w)
-                            }
-                            r.defer = !0;
-                            for (const x of [localStorage, sessionStorage]) Object.keys(x || {}).filter((z => z
-                                .startsWith("_zaraz_"))).forEach((y => {
-                                try {
-                                    j[l]["z_" + y.slice(7)] = JSON.parse(x.getItem(y))
-                                } catch {
-                                    j[l]["z_" + y.slice(7)] = x.getItem(y)
-                                }
-                            }));
-                            r.referrerPolicy = "origin";
-                            r.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(j[l])));
-                            q.parentNode.insertBefore(r, q)
-                        };
-                        ["complete", "interactive"].includes(k.readyState) ? zaraz.init() : j.addEventListener(
-                            "DOMContentLoaded", zaraz.init)
-                    }
-                }(w, d, "zarazData", "script");
-                window.zaraz._p = async bs => new Promise((bt => {
-                    if (bs) {
-                        bs.e && bs.e.forEach((bu => {
-                            try {
-                                const bv = d.querySelector("script[nonce]"),
-                                    bw = bv?.nonce || bv?.getAttribute("nonce"),
-                                    bx = d.createElement("script");
-                                bw && (bx.nonce = bw);
-                                bx.innerHTML = bu;
-                                bx.onload = () => {
-                                    d.head.removeChild(bx)
-                                };
-                                d.head.appendChild(bx)
-                            } catch (by) {
-                                console.error(`Error executing script: ${bu}\n`, by)
-                            }
-                        }));
-                        Promise.allSettled((bs.f || []).map((bz => fetch(bz[0], bz[1]))))
-                    }
-                    bt()
-                }));
-                zaraz._p({
-                    "e": ["(function(w,d){})(window,document)"]
-                });
-            })(window, document)
-        } catch (e) {
-            throw fetch("/cdn-cgi/zaraz/t"), e;
-        };
-    </script>
 </head>
 
 <body class="mx-4">

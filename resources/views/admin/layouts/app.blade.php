210a0fee531ea98ba2fd804e307a5850ee65130a
Dika Wicaksono <dhkwck00@gmail.com>
2025-02-03 04:27:38 +0700

    Update Bootstrap to version 5.3 and add CSRF token meta tag for improved security

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,9 +4,14 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | Dashboard</title>
 
+
+    <!--Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -19,104 +24,6 @@
     <!-- overlayScrollbars -->
     <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
-    <script data-cfasync="false" nonce="47407772-8439-4371-a211-058fad88f43b">
-        try {
-            (function(w, d) {
-                ! function(j, k, l, m) {
-                    if (j.zaraz) console.error("zaraz is loaded twice");
-                    else {
-                        j[l] = j[l] || {};
-                        j[l].executed = [];
-                        j.zaraz = {
-                            deferred: [],
-                            listeners: []
-                        };
-                        j.zaraz._v = "5850";
-                        j.zaraz._n = "47407772-8439-4371-a211-058fad88f43b";
-                        j.zaraz.q = [];
-                        j.zaraz._f = function(n) {
-                            return async function() {
-                                var o = Array.prototype.slice.call(arguments);
-                                j.zaraz.q.push({
-                                    m: n,
-                                    a: o
-                                })
-                            }
-                        };
-                        for (const p of ["track", "set", "debug"]) j.zaraz[p] = j.zaraz._f(p);
-                        j.zaraz.init = () => {
-                            var q = k.getElementsByTagName(m)[0],
-                                r = k.createElement(m),
-                                s = k.getElementsByTagName("title")[0];
-                            s && (j[l].t = k.getElementsByTagName("title")[0].text);
-                            j[l].x = Math.random();
-                            j[l].w = j.screen.width;
-                            j[l].h = j.screen.height;
-                            j[l].j = j.innerHeight;
-                            j[l].e = j.innerWidth;
-                            j[l].l = j.location.href;
-                            j[l].r = k.referrer;
-                            j[l].k = j.screen.colorDepth;
-                            j[l].n = k.characterSet;
-                            j[l].o = (new Date).getTimezoneOffset();
-                            if (j.dataLayer)
-                                for (const t of Object.entries(Object.entries(dataLayer).reduce(((u, v) => ({
-                                        ...u[1],
-                                        ...v[1]
-                                    })), {}))) zaraz.set(t[0], t[1], {
-                                    scope: "page"
-                                });
-                            j[l].q = [];
-                            for (; j.zaraz.q.length;) {
-                                const w = j.zaraz.q.shift();
-                                j[l].q.push(w)
-                            }
-                            r.defer = !0;
-                            for (const x of [localStorage, sessionStorage]) Object.keys(x || {}).filter((z => z
-                                .startsWith("_zaraz_"))).forEach((y => {
-                                try {
-                                    j[l]["z_" + y.slice(7)] = JSON.parse(x.getItem(y))
-                                } catch {
-                                    j[l]["z_" + y.slice(7)] = x.getItem(y)
-                                }
-                            }));
-                            r.referrerPolicy = "origin";
-                            r.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(j[l])));
-                            q.parentNode.insertBefore(r, q)
-                        };
-                        ["complete", "interactive"].includes(k.readyState) ? zaraz.init() : j.addEventListener(
-                            "DOMContentLoaded", zaraz.init)
-                    }
-                }(w, d, "zarazData", "script");
-                window.zaraz._p = async bs => new Promise((bt => {
-                    if (bs) {
-                        bs.e && bs.e.forEach((bu => {
-                            try {
-                                const bv = d.querySelector("script[nonce]"),
-                                    bw = bv?.nonce || bv?.getAttribute("nonce"),
-                                    bx = d.createElement("script");
-                                bw && (bx.nonce = bw);
-                                bx.innerHTML = bu;
-                                bx.onload = () => {
-                                    d.head.removeChild(bx)
-                                };
-                                d.head.appendChild(bx)
-                            } catch (by) {
-                                console.error(`Error executing script: ${bu}\n`, by)
-                            }
-                        }));
-                        Promise.allSettled((bs.f || []).map((bz => fetch(bz[0], bz[1]))))
-                    }
-                    bt()
-                }));
-                zaraz._p({
-                    "e": ["(function(w,d){})(window,document)"]
-                });
-            })(window, document)
-        } catch (e) {
-            throw fetch("/cdn-cgi/zaraz/t"), e;
-        };
-    </script>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
@@ -141,8 +48,10 @@
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
-    <!-- Bootstrap 4 -->
-    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
     <!-- overlayScrollbars -->
     <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->

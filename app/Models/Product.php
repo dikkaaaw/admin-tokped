5019e784a9763fa2af7bfbb37e40a9bb22efb48a
Dika Wicaksono <dhkwck00@gmail.com>
2025-02-03 04:26:52 +0700

    Add filter method to Product model for search functionality

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,11 @@ class Product extends Model
 {
     protected $table = 'products';
     protected $fillable = ['name', 'price', 'category', 'stock', 'image'];
+
+    public function filter($filter = [])
+    {
+        return $this->when(isset($filter['search']), function ($query) use ($filter) {
+            return $query->where('name', 'like', "%$filter[search]%");
+        });
+    }
 }

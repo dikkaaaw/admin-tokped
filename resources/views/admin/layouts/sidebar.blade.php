35bc29fb72275a92251f1de7b7ffd07fbef79921
Dika Wicaksono <dhkwck00@gmail.com>
2025-02-03 04:27:45 +0700

    Update sidebar layout: change brand logo, display user name dynamically, and update navigation links

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,20 +1,21 @@
  <!-- Main Sidebar Container -->
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
      <!-- Brand Logo -->
-     <a href="#" class="brand-link">
-         {{-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-             style="opacity: .8"> --}}
-         <span class="brand-text font-weight-light">AdminLTE 3</span>
+     <a href="/" class="brand-link text-decoration-none">
+         <img src="{{ asset('dist/assets/img/logo/logo.jpg') }}" alt="Logo" class=""
+             style="opacity: 0.8; max-width: 40px;">
+         <span class="brand-text fw-light ms-2">Dewata Melon</span>
      </a>
 
      <!-- Sidebar -->
      <div class="sidebar">
          <div class="pb-3 mt-3 mb-3 user-panel d-flex">
              <div class="image">
-                 <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                 <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}&background=random"
+                     class="img-circle" alt="User Image">
              </div>
              <div class="info">
-                 <a href="#" class="d-block">Alexander Pierce</a>
+                 <span class="text-white d-block">{{ Auth::user()->name }}</span>
              </div>
          </div>
 
@@ -62,15 +63,9 @@
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="#" class="nav-link">
+                             <a href="{{ URL('admin/product/add') }}" class="nav-link">
                                  <i class="fas fa-plus nav-icon"></i>
                                  <p>Tambah Product</p>
-                             </a>
-                         </li>
-                         <li class="nav-item">
-                             <a href="#" class="nav-link">
-                                 <i class="fas fa-edit nav-icon"></i>
-                                 <p>Edit Product</p>
                              </a>
                          </li>
                      </ul>

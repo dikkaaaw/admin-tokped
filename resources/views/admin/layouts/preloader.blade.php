324710b77c62842118afb0a3688d516f0206d381
Dika Wicaksono <dhkwck00@gmail.com>
2025-02-03 14:06:16 +0700

    Update preloader logo: change logo file format from JPG to PNG for better quality

--- resources/views/admin/layouts/preloader.blade.php
+++ resources/views/admin/layouts/preloader.blade.php
@@ -1,5 +1,5 @@
 <!-- Preloader -->
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('dist/assets/img/logo/logo.jpg') }}" alt="logo" height="60"
+    <img class="animation__shake" src="{{ asset('dist/assets/img/logo/logo.png') }}" alt="logo" height="60"
         width="60">
 </div>

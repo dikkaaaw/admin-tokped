bb097a22259cd2ef643ab88a227ef2ca4bc4e389
ishala <faishalali22@students.amikom.ac.id>
2025-02-06 19:48:14 +0700

    change favicon

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FoodMart</title>
+    <title>Dewata Melon</title>
 
     {{-- Font Awesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">

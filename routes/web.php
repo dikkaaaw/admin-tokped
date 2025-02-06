2a604d0fc17c523b85f8294eddf0f77e2c260b59
Dika Wicaksono <dhkwck00@gmail.com>
2025-02-06 17:53:53 +0700

    Add route for updating user address in authentication controller

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('login', 'App\Http\Controllers\AuthController@login_page')->name('login_page');
 Route::post('logout', 'App\Http\Controllers\AuthController@logout')->name('logout');
 Route::post('login', 'App\Http\Controllers\AuthController@login')->name('login');
+
+// Update Address
 Route::post('update-address', 'App\Http\Controllers\AuthController@updateAddress')->name('updateAddress');
 
 // Register
